2f52db39c8309958012b32d3c7a64a70e9cad3fb
synodriver <624805065@qq.com>
2020-05-24 11:15:49 +0800

    fix bug

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import aioaria2
-import threading
 from queue import Queue
 
 q = asyncio.Queue()
@@ -28,12 +27,6 @@ def callback3(future):
     print("自动重新下载")
 
 
-def onresult(future):
-    data = future.result()["result"]
-    url = data[0]["uri"]
-    failed_url.put_nowait(url)
-
-
 async def get_trigger():
     client = await aioaria2.Aria2WebsocketTrigger.create("id", "http://192.168.0.107:6800/jsonrpc", "batch",
                                                          token="admin", queue=q)
@@ -41,7 +34,6 @@ async def get_trigger():
     client.onDownloadStart(callback)
     client.onDownloadComplete(callback2)
     client.onDownloadError(callback3)
-    client.onResullt(onresult)
     await client.start_ws()
 
 
@@ -51,7 +43,7 @@ async def get_client():
     print("success init")
     # print(failed_gid._qsize())
     while True:
-        gid=await failed_gid.get()
+        gid = await failed_gid.get()
         print("取得gid:{0}".format(gid))
         data = await client.getFiles(gid)
         url = data[0]["uris"][0]["uri"]
@@ -61,22 +53,11 @@ async def get_client():
         print("重新入队成功:{0}".format(gid))
 
 
-def start_loop(loop: asyncio.AbstractEventLoop):
-    asyncio.set_event_loop(loop)
-    task = loop.create_task(get_trigger())
-    loop.run_until_complete(task)
-
-
 def main():
     loop = asyncio.get_event_loop()
     tasks = [loop.create_task(get_trigger()), loop.create_task(get_client())]
     loop.run_until_complete(asyncio.wait(tasks))
-    # loop_thread = threading.Thread(target=start_loop, args=(asyncio.new_event_loop(),))
-    # loop_thread.start()
-    # task = loop.create_task(get_client())
-    # loop.run_until_complete(task)
 
 
 if __name__ == "__main__":
     main()
-
